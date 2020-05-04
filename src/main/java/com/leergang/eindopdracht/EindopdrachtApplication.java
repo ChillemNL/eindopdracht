becb2cd89c188afa7404b5569a54cd5cc4397d0f
Willem Holthuis <willem.holthuis@gmail.com>
2020-05-04 08:19:28 +0200

    Initiele commit

---
+++ src/main/java/com/leergang/eindopdracht/EindopdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.leergang.eindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtApplication.class, args);
+	}
+
+}
