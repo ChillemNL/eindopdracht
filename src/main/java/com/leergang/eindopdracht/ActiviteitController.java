cd70d783014ba295f87bace9594a5f7d7af47c00
Willem Holthuis <willem.holthuis@gmail.com>
2020-05-31 10:31:26 +0200

    Eindpunt voor ophalen activiteiten per categorie toegevoegd.

--- src/main/java/com/leergang/eindopdracht/ActiviteitController.java
+++ src/main/java/com/leergang/eindopdracht/ActiviteitController.java
@@ -1,20 +1,12 @@
 package com.leergang.eindopdracht;
 
 import com.leergang.eindopdracht.models.Activiteit;
-import com.leergang.eindopdracht.models.Categorie;
 
 import com.leergang.eindopdracht.repositories.ActiviteitRepository;
-import com.leergang.eindopdracht.repositories.CategorieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class ActiviteitController {
@@ -55,5 +47,6 @@ public class ActiviteitController {
         return activiteitRepository.findAll();
     }
 
-
+    @GetMapping("/activieit_ophalen_per_categorie/{id}")
+    @ResponseBody public  Iterable<Activiteit> getActivities(@PathVariable("id") Integer id){return activiteitRepository.findBycategorieId(id);}
 }
--- src/main/java/com/leergang/eindopdracht/repositories/ActiviteitRepository.java
+++ src/main/java/com/leergang/eindopdracht/repositories/ActiviteitRepository.java
@@ -1,7 +1,14 @@
 package com.leergang.eindopdracht.repositories;
 
 import com.leergang.eindopdracht.models.Activiteit;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ActiviteitRepository extends CrudRepository<Activiteit, Integer> {
+
+    List<Activiteit> findBycategorieId(Integer categorieId);
+
 }
