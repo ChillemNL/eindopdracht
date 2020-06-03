61d17ac755cbe266d6e1d78787a39e08fa851903
Andrea van der Pol <andreavanderpol@live.nl>
2020-06-03 17:08:14 +0200

    Aanpassen activiteit toegevoegd

--- src/main/java/com/leergang/eindopdracht/ActiviteitController.java
+++ src/main/java/com/leergang/eindopdracht/ActiviteitController.java
@@ -3,16 +3,23 @@ package com.leergang.eindopdracht;
 import com.leergang.eindopdracht.models.Activiteit;
 
 import com.leergang.eindopdracht.repositories.ActiviteitRepository;
+import com.leergang.eindopdracht.repositories.CategorieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import com.leergang.eindopdracht.models.Categorie;
+
+import java.util.Optional;
 
 @Controller
 public class ActiviteitController {
 
     @Autowired
     private ActiviteitRepository activiteitRepository;
+
+    @Autowired
+    private CategorieRepository categorieRepository;
 
     @GetMapping("/")
     public String foo() {
@@ -54,4 +61,47 @@ public class ActiviteitController {
 
     @GetMapping("/activieit_ophalen_per_categorie/{id}")
     @ResponseBody public  Iterable<Activiteit> getActivities(@PathVariable("id") Integer id){return activiteitRepository.findBycategorieId(id);}
+
+    @GetMapping("activiteit_aanpassen/{id}")
+    public String aanpassenFormulier(Model model, @PathVariable String id) {
+        Iterable<Categorie> categories = categorieRepository.findAll();
+        model.addAttribute("categories", categories);
+
+        Optional<Activiteit> query = activiteitRepository.findById(Integer.parseInt(id));
+        if (query.isPresent()){
+            Activiteit activiteit = query.get();
+            model.addAttribute("activiteit", activiteit);
+            return "activiteit_aanpassen";
+        }
+        return "index";
+        // De functie moet de beheerpagina hier returnen.
+    }
+
+    @PostMapping("activiteit_aanpassen/{id}")
+    public String activiteitAanpassen(@ModelAttribute ActiviteitRequest request, @PathVariable String id) {
+        // model ophalen
+        // velden van de model aanpassen met getters en setters
+        // gegevens van model opslaan
+
+        Optional<Activiteit> query = activiteitRepository.findById(Integer.parseInt(id));
+       if (query.isPresent()) {
+           Activiteit activiteit = query.get();
+           activiteit.setCategorieId(request.getCategorieId());
+           activiteit.setNaamOrganisatie(request.getNaamOrganisatie());
+           activiteit.setPlaats(request.getPlaats());
+           activiteit.setRegio(request.getRegio());
+           activiteit.setNaamActiviteit(request.getNaamActiviteit());
+           activiteit.setBeschrijvingActiviteit(request.getBeschrijvingActiviteit());
+           activiteit.setKosten(request.getKosten());
+           activiteit.setLeeftijdscategorie(request.getLeeftijdscategorie());
+           activiteit.setSoortActiviteit(request.getSoortActiviteit());
+           activiteit.setWebsite(request.getWebsite());
+           activiteitRepository.save(activiteit);
+       }
+            // Hier moet de beheerpagina worden gereturned.
+        return "resultaat";
+    }
+
 }
+
+
