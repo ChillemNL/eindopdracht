ea5aecde555988525c830ea07f5070fb65f59287
Willem Holthuis <willem.holthuis@gmail.com>
2020-06-07 13:01:31 +0200

    Test op laden index toegevoegd.

--- src/test/java/com/leergang/eindopdracht/EindopdrachtApplicationTests.java
+++ src/test/java/com/leergang/eindopdracht/EindopdrachtApplicationTests.java
@@ -1,13 +1,28 @@
 package com.leergang.eindopdracht;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
-class EindopdrachtApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EindopdrachtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+		@LocalServerPort
+		private int port;
+
+		@Autowired
+		private TestRestTemplate restTemplate;
+
+		@Test //controleert of index.html terugkomt door de titel op de pagina te zoeken
+		public void greetingShouldReturnDefaultMessage() throws Exception {
+			assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+					String.class)).contains("ActiviteitenTip");
+		}
+
 
 }
+
+
